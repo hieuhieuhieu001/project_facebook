ca9220a9b745d7a07e98d9f940219756425e403e
hieuhieuhieu001 <86778988+hieuhieuhieu001@users.noreply.github.com>
2022-07-28 16:20:00 +0700

    Add files via upload

---
+++ project_facebook/css/index.js
@@ -0,0 +1,14 @@
+function myFuction(){
+    document.getElementById('messenger').style.display='block';
+    document.getElementById('fun-user-box').style.display='none';
+}
+function messenger_bt_exit(){
+    document.getElementById('messenger').style.display='none';
+}
+function userBox(){
+    document.getElementById('fun-user-box').style.display='block';
+    document.getElementById('messenger').style.display='none';
+}
+function userbox_bt_exit(){
+    document.getElementById('fun-user-box').style.display='none'
+}
